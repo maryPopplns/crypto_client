90dbed4f40b20c317fd9dbd89b874bc99471d028
Spencer Knight <spencer.knightusn@gmail.com>
2022-06-06 16:50:41 -0700

    positioned dashboard wrapper

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,19 +1,11 @@
 import './landingPage.css';
 import React, { useState } from 'react';
+import Button from '@mui/material/Button';
 import { Routes, Route } from 'react-router-dom';
-import ethereum from './assets/Ethereum.png';
-import bitcoin from './assets/Bitcoin.png';
-import dashboard from './assets/Dashboard.png';
 
-// import binance from './assets/Binance.png';
-// import decrease from './assets/Decrease.png';
-// import dogecoin from './assets/Dogecoin.png';
-// import increase from './assets/Increase.png';
-// import litecoin from './assets/Litecoin.png';
-// import solana from './assets/Solana.png';
-// import tether from './assets/Tether.png';
-// import usdCoin from './assets/USDCoin.png';
-// import arm from './assets/Arm.png';
+import bitcoin from './assets/Bitcoin.png';
+import ethereum from './assets/Ethereum.png';
+import dashboard from './assets/Dashboard.png';
 
 type props = { setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>> };
 
@@ -22,10 +14,14 @@ function LandingPage({ setIsLoggedIn }: props): JSX.Element {
     <main className='landing-page'>
       <header>header</header>
       <h1 className='landing-page-heading'>Coin Tracker</h1>
-      <div className='dashboard-container'>
-        <img className='dashboard-icon' src={dashboard} alt='dashboard' />
-        <img className='ethereum-icon coin' src={bitcoin} alt='bitcoin' />
-        <img className='bitcoin-icon coin' src={ethereum} alt='ethereum' />
+      <p className='landing-page-description'>Track your crypto portfolio</p>
+      <div className='dashboard-wrapper'>
+        <div className='dashboard-container'>
+          <img className='dashboard-icon' src={dashboard} alt='dashboard' />
+          <img className='bitcoin-icon coin' src={bitcoin} alt='bitcoin' />
+          <img className='ethereum-icon coin' src={ethereum} alt='ethereum' />
+        </div>
+        <Button variant='contained'>Try It Free</Button>
       </div>
     </main>
   );
