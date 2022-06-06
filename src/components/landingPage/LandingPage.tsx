6ff1ab92b91af4cd3fa6a2195fda95e949679683
Spencer Knight <spencer.knightusn@gmail.com>
2022-06-05 20:00:10 -0700

    added assets

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,12 +1,23 @@
 import './landingPage.css';
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import binance from './assets/Binance.png';
+import bitcoin from './assets/Bitcoin.png';
+import decrease from './assets/Decrease.png';
+import dogecoin from './assets/Dogecoin.png';
+import ethereum from './assets/Ethereum.png';
+import increase from './assets/Increase.png';
+import litecoin from './assets/Litecoin.png';
+import solana from './assets/Solana.png';
+import tether from './assets/Tether.png';
+import usdCoin from './assets/USDCoin.png';
 
 type props = { setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>> };
 
 function LandingPage({ setIsLoggedIn }: props): JSX.Element {
   return (
     <main>
+      <img src={binance} alt='binance' />
       <div>landing page</div>
     </main>
   );
