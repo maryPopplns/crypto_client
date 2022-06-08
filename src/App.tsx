58cefb0e249f22f2cc27354efb13bc7cc4db891a
Spencer Knight <spencer.knightusn@gmail.com>
2022-06-07 18:49:39 -0700

    setup routes for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
 import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingPage/LandingPage';
 
 function App(): JSX.Element {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   return (
     <>
-      {!isLoggedIn && <LandingPage setIsLoggedIn={setIsLoggedIn} />}
+      {!isLoggedIn && (
+        <Routes>
+          <Route
+            path='/'
+            element={<LandingPage setIsLoggedIn={setIsLoggedIn} />}
+          />
+        </Routes>
+      )}
       {/* {isLoggedIn && <Dashboard />} */}
     </>
   );
